f42dc3b554b5a0709b8e068a109b2c334c75b2a1
zeynab <zeynab.mvs@gmail.com>
2022-08-01 18:18:34 +0430

    owl added not working

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,3 +146,22 @@
     });
 
 })(jQuery);
+
+
+jQuery(document).ready(function($) {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            }
+        },
+        dots:true,
+        // navText: ["<img src='images/icon-angle-left.svg'>","<img src='images/icon-angle-right.svg'>"],
+        smartSpeed: 1000,
+
+    });
+});
+
---
+++ assets/js/owl.js
@@ -0,0 +1,17 @@
+// jQuery(document).ready(function($) {
+//     $('.owl-carousel').owlCarousel({
+//         loop:true,
+//         responsiveClass:true,
+//         responsive:{
+//             0:{
+//                 items:1,
+//                 nav:true
+//             }
+//         },
+//         dots:true,
+//         // navText: ["<img src='images/icon-angle-left.svg'>","<img src='images/icon-angle-right.svg'>"],
+//         smartSpeed: 1000,
+
+//     });
+// });
+
