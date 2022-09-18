0d8f9f0b5da53ba17ce37765752ec3e59e61c73b
zeynab <zeynab.mvs@gmail.com>
2022-09-18 17:15:12 +0430

    style: refactor carousel styles

--- assets/owlcarousel/owl.js
+++ assets/owlcarousel/owl.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function ($) {
     const arrowLeftUrl = '/assets/images/arrow-left.svg';
     const arrowRightUrl = '/assets/images/arrow-right.svg';
 
-    $('#slider3 .owl-carousel').owlCarousel({
+    $('#homeSlider .owl-carousel').owlCarousel({
         items: 1,
         rtl: true,
         loop: true,
@@ -21,42 +21,10 @@ jQuery(document).ready(function ($) {
         margin: 10,
         animateIn: 'fadeIn', 
         animateOut: 'fadeOut',
-        dotsContainer: '#myDots',
-        navContainer: '#myNavs',
+        dotsContainer: '#customDots',
+        navContainer: '#customNavs',
     });
     
-    $('#slider2 .owl-carousel').owlCarousel({
-        items: 1,
-        rtl: true,
-        loop: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: true
-            }
-        },
-        // nav: false,
-        navText: [`<img src=${arrowRightUrl}>`, `<img src=${arrowLeftUrl}>`],
-        dots: true,
-        smartSpeed: 1000,
-        autoHeight: true, //slider height is based on each item's height
-        margin: 10,
-        animateIn: 'fadeIn', 
-        animateOut: 'fadeOut',
-    });
-
-    $('#slider1 .owl-carousel').owlCarousel({
-        items: 1,
-        rtl: true,
-        loop: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-                nav: true
-            }
-        },
-    });
+   
 });
 
