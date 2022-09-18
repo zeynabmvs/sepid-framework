406c0bcf2f89c3d91a6cdde3bf12f0534125e69e
zeynab <zeynab.mvs@gmail.com>
2022-09-18 14:23:33 +0430

    feat: owl carousel added to home page

--- assets/owlcarousel/owl.js
+++ assets/owlcarousel/owl.js
@@ -1,7 +1,28 @@
 jQuery(document).ready(function ($) {
-    const arrowLeftUrl = '/assets/images/icon-angle-left.svg';
-    const arrowRightUrl = '/assets/images/icon-angle-right.svg';
+    const arrowLeftUrl = '/assets/images/arrow-left.svg';
+    const arrowRightUrl = '/assets/images/arrow-right.svg';
 
+    $('#slider3 .owl-carousel').owlCarousel({
+        items: 1,
+        rtl: true,
+        loop: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            }
+        },
+        nav: true,
+        navText: [`<img src=${arrowRightUrl}>`, `<img src=${arrowLeftUrl}>`],
+        dots: true,
+        smartSpeed: 1000,
+        autoHeight: true, //slider height is based on each item's height
+        margin: 10,
+        animateIn: 'fadeIn', 
+        animateOut: 'fadeOut',
+    });
+    
     $('#slider2 .owl-carousel').owlCarousel({
         items: 1,
         rtl: true,
